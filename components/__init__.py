b369d53a8413c201a05beeeab44929abfa16895e
pdicenso1-cloud <pdicenso1@gmail.com>
2026-08-16 10:23:56 -0400

    Update __init__.py

--- components/__init__.py
+++ components/__init__.py
@@ -3,7 +3,12 @@
 from components.bottom_sheet import render_bottom_sheet
 from components.draft_board import render_draft_board
 from components.draft_header import render_compact_draft_header
-from components.draft_room import DraftRoomDependencies, render_draft_room
+from components.draft_room import (
+    DraftRoomDependencies,
+    render_draft_room,
+    render_header_and_board,
+    render_tray,
+)
 
 __all__ = [
     "DraftRoomDependencies",
@@ -11,4 +16,6 @@ __all__ = [
     "render_compact_draft_header",
     "render_draft_board",
     "render_draft_room",
+    "render_header_and_board",
+    "render_tray",
 ]
